25220fce9ae3fc646c44f6656408e5042f5ef16e
markke78 <markke78@hotmail.com>
2022-09-16 14:14:32 -0700

    fixed enter

--- 1205-Sep2022/1205-Sep2022/Quiz2TsungChihKe.cpp
+++ 1205-Sep2022/1205-Sep2022/Quiz2TsungChihKe.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 void Week2Quiz2() {
-	int numbers[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int numbers[10];
 	int sum = 0;
 	for (int i = 0; i < 10; i++) {
-		std::cout << numbers[i] << "\n";
+		std::cout << "Please enter your numbers: " << "\n";
+		std::cin >> numbers[i];
 		sum += numbers[i];
+	}
+	for (int i = 0; i < 10; i++) {
+		std::cout << numbers[i] << "\n";
 	}
 	int getArrayLength = sizeof(numbers) / sizeof(int);
 	int average = sum / getArrayLength;
